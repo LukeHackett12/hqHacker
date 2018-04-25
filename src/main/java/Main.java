a4223129d30ef884d85c8321f40b88099c063ce0
Luke Hackett <lhackett@tcd.ie>
2018-04-25 01:22:08 +0100

    First "working"?? version of the advanced googler...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.lowagie.text.Paragraph;
 import org.jsoup.*;
@@ -30,50 +32,44 @@ public class Main {
     }
 
     public static void search() throws IOException, AWTException {
-        BufferedImage question = new Robot().createScreenCapture(new Rectangle(10, 300, 850, 975));
+        BufferedImage question = new Robot().createScreenCapture(new Rectangle(10, 300, 700, 975));
         ImageIO.write(question, "png", new File("question.png"));
-/*
-        BufferedImage answer = new Robot().createScreenCapture(new Rectangle(10, 775, 850, 500));
-        ImageIO.write(answer, "png", new File("answer.png"));
-*/
+
         File questionImage = new File("question.png");
-        /*
-        File answerImage = new File("answer.png");
-        */
         ITesseract instance = new Tesseract();  // JNA Interface Mapping
 
         try {
-            String fullString = instance.doOCR(questionImage).replaceAll("\n", " ");
-            String questionString = fullString.split("\\?")[0];
+            String fullString = instance.doOCR(questionImage);
+            String questionString = (fullString.split("\\?")[0] + "?").replaceAll("\n", " ");
             System.out.println("Question: " + questionString);
-            String[] answerStrings = fullString.split("\\?")[1].split(" ");
+            String[] answerStrings = fullString.split("\\?")[1].split("\n");
 
             String[] answerStringsConcated = new String[3];
+
             int j = 0;
             for(int i = 0; i < answerStrings.length; i++){
                 if(!answerStrings[i].equals("")) {
+                    if(j == 3) break;
                     answerStringsConcated[j] = answerStrings[i];
                     j++;
                 }
             }
 
+            System.out.println("Possible Answers: ");
             for (String s : answerStringsConcated) {
                 System.out.println(s);
             }
-            /*
-            String answerString = instance.doOCR(answerImage).replaceAll("\n", " ");
-            System.out.println("Answers: " + answerString);
-            */
-/*
-            //BingSearch bing = new BingSearch();
-            // bing.Search(result);
 
-            URL url = new URL("https://www.google.com/search?q=" + result.replaceAll(" ", "+").replaceAll("\"", ""));
-            //System.out.println(url);
+            URL url = new URL(("https://www.googleapis.com/customsearch/v1?q=" + questionString.replaceAll(" ", "+").replaceAll("\"", "").split("which")[0] + "?" +
+                        "&cx=016409237003735062340%3Anfsrbopx80k" +
+                        "&orTerms=" + answerStringsConcated[0].replaceAll(" ", "+") + "," + answerStringsConcated[1].replaceAll(" ", "+") + "," + answerStringsConcated[2].replaceAll(" ", "+") +
+                        "&key=AIzaSyBMsx3u8GCtyYT1akAv0zRNiQuxuxQJt1I").replaceAll(" ", "%20"));
+
+            System.out.println(url);
 
             URLConnection urlConnect = url.openConnection();
             urlConnect.addRequestProperty("User-Agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+                    "Mozilla");
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(
                             urlConnect.getInputStream()));
@@ -84,33 +80,65 @@ public class Main {
                 inputSaved.append(inputLine);
             in.close();
 
-            //String pattern = "(?i)(<title.*?>)(.+?)()";
-            //String updated = inputSaved.toString().replaceAll(pattern, "$2");
-            //System.out.println(inputSaved);
-            Document doc = Jsoup.parseBodyFragment(inputSaved.toString());
-            Elements links = doc.select("a[href]");
+            //Start count
 
-            //System.out.println(doc);
+            int highest = 0;
+            int whichHigh = 0;
 
-            for (int i = 31; i < 40; i++) {
-                Element l = links.get(i);
-                try {
-                    String linkHref = l.attr("href").substring(0, l.attr("href").indexOf("&")).replaceAll("(http:\\/\\/webcache\\.googleusercontent\\.com\\/.*?:.*?:)", "");
-                    linkHref = linkHref.replace("/url?q=", "");
-                    System.out.println(linkHref);
-                    //java.awt.Desktop.getDesktop().browse(new URI(linkHref));
-                } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Nothing");
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            Pattern p = Pattern.compile(answerStringsConcated[0].toLowerCase());
+            Matcher m = p.matcher(inputSaved.toString().toLowerCase());
+            int i = 0;
+            while (m.find()) {
+                i++;
             }
+            p = Pattern.compile(answerStringsConcated[0].toLowerCase().substring(0, answerStringsConcated[0].length()-1));
+            m = p.matcher(inputSaved.toString().toLowerCase());
+            while (m.find()) {
+                i++;
+            }
+            if(i >= highest){
+                whichHigh = 0;
+                highest = i;
+            }
+            System.out.println("Answer 1 instances: " + i);
 
-*/
+            p = Pattern.compile(answerStringsConcated[1].toLowerCase());
+            m = p.matcher(inputSaved.toString().toLowerCase());
+            i = 0;
+            while (m.find()) {
+                i++;
+            }
+            p = Pattern.compile(answerStringsConcated[1].toLowerCase().substring(0, answerStringsConcated[1].length()-1));
+            m = p.matcher(inputSaved.toString().toLowerCase());
+            while (m.find()) {
+                i++;
+            }
+            if(i >= highest){
+                whichHigh = 1;
+                highest = i;
+            }
+            System.out.println("Answer 2 instances: " + i);
+
+            p = Pattern.compile(answerStringsConcated[2].toLowerCase());
+            m = p.matcher(inputSaved.toString().toLowerCase());
+            i = 0;
+            while (m.find()) {
+                i++;
+            }
+            p = Pattern.compile(answerStringsConcated[2].toLowerCase().substring(0, answerStringsConcated[2].length()-1));
+            m = p.matcher(inputSaved.toString().toLowerCase());
+            while (m.find()) {
+                i++;
+            }
+            if(i >= highest){
+                whichHigh = 2;
+                highest = i;
+            }
+            System.out.println("Answer 3 instances: " + i);
+
+            String ansString = answerStringsConcated[whichHigh];
+            System.out.println("The answer is probably \"" + ansString + "\"");
+
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
